ceeb088311cc0ac815f3b924499afaa8dc1de2e5
idrsiozdmrr <75699502+idrsiozdmrr@users.noreply.github.com>
2021-01-29 20:11:41 +0300

    Delete ayarlar.js

--- ayarlar.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    token: "NzQ3NzM4NDU5MTA3NjIyOTUy.X0TPtA.awcFB3OsXe2ZN21XWu2_FndPtY4",
-    prefix: "e!",
-    admins: [
-        "636573504598442084",
-      "para ekleyecek kişi idsi"
-]}
